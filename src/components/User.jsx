a424e3116af7fae4234fe1ecdfa0c7523aa439db
Dan <dmann@westervelt.com>
2024-02-26 11:07:43 -0600

    Cleaning up

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useStore } from '@nanostores/react';
 
 import { $users, addUser, deleteUser } from '../store/users.js';
@@ -41,22 +41,27 @@ const User = () => {
     return (
         <>
             <div className='flex flex-col'>
-                <div className='mb-4 lg:[&:nth-child()]:ml-4'><input className='text-black' onChange={(event) => handleChange(event, 'First')}></input><label className='ml-4'>First Name</label></div>
-                <div className='mb-4'><input className='text-black' onChange={(event) => handleChange(event, 'Last')}></input><label className='ml-4'>Last Name</label></div>
-                <div className='mb-4'><input className='text-black' onChange={(event) => handleChange(event, 'Department')}></input><label className='ml-4'>Department</label></div>
+                <div className='mb-4 lg:[&:nth-child()]:ml-4'>
+                    <input className='text-black' onChange={(event) => handleChange(event, 'First')}></input><label className='ml-4'>First Name</label>
+                </div>
+                <div className='mb-4'>
+                    <input className='text-black' onChange={(event) => handleChange(event, 'Last')}></input><label className='ml-4'>Last Name</label>
+                </div>
+                <div className='mb-4'>
+                    <input className='text-black' onChange={(event) => handleChange(event, 'Department')}></input><label className='ml-4'>Department</label>
+                </div>
             </div>
             <div className='mb-8 mt-4 border-2 border-white p-2 rounded-lg flex flex-col items-start'>
                 <div className='w-full'>
-                {Object.keys(user).map((element, i) => (
-                    <div className='flex' key={i}><div className='w-1/4'>{`${Object.keys(user)[i]} Name: `}</div><div className='text-green-300 ml-8'>{user[element]}</div></div>
-                ))}
+                    {Object.keys(user).map((element, i) => (
+                        <div className='flex' key={i}><div className='w-1/4'>{`${Object.keys(user)[i]} Name: `}</div><div className='text-green-300 ml-8'>{user[element]}</div></div>
+                    ))}
                 </div>
             </div>
             <div className='flex justify-center'>
-            <button className='border-2 border-green-300 rounded-lg p-2 mr-4' onClick={handleUpdate}>Add User</button>
-            <button className='border-2 border-green-300 rounded-lg p-2 ml-4' onClick={handlePersist}>Persist User</button>
+                <button className='border-2 border-green-300 rounded-lg p-2 mr-4' onClick={handleUpdate}>Add User</button>
+                <button className='border-2 border-green-300 rounded-lg p-2 ml-4' onClick={handlePersist}>Persist User</button>
             </div>
-            
 
             <div className='border-t-2 border-b-2 pt-4 border-gray-500 mt-8'>
                 {
